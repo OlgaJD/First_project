6b1fe20176fed4d21645c76f22cea18f2f6b0ed1
Olga <lelya.zy@gmail.com>
2022-08-19 09:34:01 +0300

    example_003_random

--- Example_003_Sum/Program.cs
+++ Example_003_Sum/Program.cs
@@ -1,6 +1,6 @@
 ﻿int numberA = new Random().Next(1, 10);// 0,1,2,3...9
-Console.Write( numberA );
+Console.WriteLine(numberA);
 int numberB = new Random().Next(1, 10);
-Console.Write( numberB );
+Console.WriteLine(numberB);
 int result = numberA+numberB;
 Console.WriteLine(result);
