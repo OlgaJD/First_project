c196b7a5eee04d212a9f550e0f4650426d04c085
Olga <lelya.zy@gmail.com>
2022-08-19 10:05:47 +0300

    example_005_ConditionIfElse

---
+++ Example_004_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя ");
+string username = Console.ReadLine ();
+if(username.ToLower () == "оля")
+{
+    Console.Write("Привееет!!!, ");
+    Console.WriteLine (username);
+}
+else
+{
+    Console.Write("Опять ты, ");
+    Console.WriteLine (username);
+}
